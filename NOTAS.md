0bd4a53caa1f956d4bb7691558d8055e9707a7da
Rubén Chavarría <txingo@gmail.com>
2019-02-23 16:57:56 +0100

    Comment hardcoded TEST_BASE_URL

---
+++ NOTAS.md
@@ -0,0 +1,15 @@
+# MIS NOTAS
+
+serverless es:
+
+- event driven
+- utility based (si no lo utilizas no pagas)
+- stateless
+- code execution environment
+
+## Step 1
+
+**Mucho cuidado con las variables de entorno**. En Windows, y con npm, parece que no puedo setear las
+vbls de entorno para decidir qué tipo de tests ejecuto (acceptance -> tira de http, 
+integration -> tira de algún tipo de mock) 
+
--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,6 +1,6 @@
 module.exports.init = () => {
   process.env.mastersTable = `masters-${process.env.TEST_STAGE}`;
-  process.env.TEST_BASE_URL = `https://bbhgpea36h.execute-api.eu-west-1.amazonaws.com/${process.env.TEST_STAGE}/api`;
+  // process.env.TEST_BASE_URL = `https://bbhgpea36h.execute-api.eu-west-1.amazonaws.com/${process.env.TEST_STAGE}/api`;
   process.env.AWS_REGION = "eu-west-1";
 
   console.log('Variables de entorno');
