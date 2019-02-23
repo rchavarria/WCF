a7cac8bcb9f5b0267c69d74706a4d38605057a39
Rubén Chavarría <txingo@gmail.com>
2019-02-23 16:54:46 +0100

    Add CircleCI config

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -1,11 +1,12 @@
 module.exports.init = () => {
-  process.env.mastersTable = "masters-devrchavarria";
-  process.env.TEST_BASE_URL = "https://bbhgpea36h.execute-api.eu-west-1.amazonaws.com/devrchavarria/api";
+  process.env.mastersTable = `masters-${process.env.TEST_STAGE}`;
+  process.env.TEST_BASE_URL = `https://bbhgpea36h.execute-api.eu-west-1.amazonaws.com/${process.env.TEST_STAGE}/api`;
   process.env.AWS_REGION = "eu-west-1";
 
   console.log('Variables de entorno');
   console.log('TEST_BASE_URL: ', process.env.TEST_BASE_URL);
   console.log('TEST_MODE; ', process.env.TEST_MODE);
+  console.log('TEST_STAGE; ', process.env.TEST_STAGE);
   console.log('SLSUSER; ', process.env.SLSUSER);
   console.log('AWS_PROFILE: ', process.env.AWS_PROFILE);
 };
